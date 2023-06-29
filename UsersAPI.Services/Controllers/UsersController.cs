593a0a494476ff7792172af6c71e827297ddd103
thiagoabr <thiagoabr@gmail.com>
2023-06-28 22:05:16 -0300

    first commit

---
+++ UsersAPI.Services/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        /// <summary>
+        /// Criar conta de usuário
+        /// </summary>
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Alterar os dados da conta do usuário
+        /// </summary>
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Excluir conta de usuário
+        /// </summary>
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Consultar os dados da conta do usuário
+        /// </summary>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
